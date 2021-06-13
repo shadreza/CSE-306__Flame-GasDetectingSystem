232e9bb4e9acf10e573531f0dba2cb74545df818
shadreza <45334134+shadreza@users.noreply.github.com>
2021-06-14 02:20:44 +0600

    Added the mq4 gas sensor also

--- project-306/code/sketch_jun13a/sketch_jun13a.ino
+++ project-306/code/sketch_jun13a/sketch_jun13a.ino
@@ -1,11 +1,29 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(8, 6, 5, 9, 3, 2);
+
+#define MQPin A0
+#define buzzer 1
+
+
 int LED = 7;
 int flame_sensor = 4;
 int flame;
+
 
 void setup()
 {
   pinMode(7, OUTPUT);
   pinMode(4, INPUT);
+  lcd.begin(16, 2);
+   pinMode(MQPin, INPUT_PULLUP);
+   pinMode(buzzer, OUTPUT);
+    lcd.setCursor(5, 0);
+    lcd.print("GAS");
+    lcd.setCursor(3, 1);
+    lcd.print("DETECTOR");
+    delay(1000);
+    lcd.clear();
 }
 void loop()
 {
@@ -13,9 +31,36 @@ void loop()
   if(flame == 1)
   {
     digitalWrite(LED, HIGH);
+    digitalWrite(buzzer, HIGH);
+    lcd.setCursor(6, 0);
+    lcd.print("GAS");
+     lcd.setCursor(3, 1);
+    lcd.print("DETECTED");
+    delay(200);
+    lcd.clear();
+    delay(200);
   }
   else
   {
     digitalWrite(LED, LOW);
   }
+  int gas_value = digitalRead(MQPin);
+
+if(gas_value==HIGH)
+{
+  digitalWrite(buzzer, HIGH);
+  lcd.setCursor(6, 0);
+  lcd.print("GAS");
+   lcd.setCursor(3, 1);
+  lcd.print("DETECTED");
+  delay(200);
+  lcd.clear();
+  delay(200);
+  
+}
+else
+{
+ lcd.clear();
+ digitalWrite(buzzer, LOW); 
+}
 }
